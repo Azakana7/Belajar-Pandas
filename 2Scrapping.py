0caaca76a0155550eb3dc300d45163a9125d92f7
Azakana <danielsaputradivo@gmail.com>
2024-01-23 05:36:06 +0700

    Membetulkan kode yang salah

--- 2Scrapping.py
+++ 2Scrapping.py
@@ -4,35 +4,29 @@ from bs4 import BeautifulSoup
 
 th = "http://www.jobs.id/lowongan-kerja?kata-kunci=part time"
 halaman = requests.get(th)
-hasil = BeautifulSoup(halaman.content, 'html-parser')
+hasil = BeautifulSoup(halaman.content, 'html.parser')
 lowkers = hasil.find_all(class_="single-jobs-ads")
 
 posisi = []
 instansi = []
 gaji = []
 for p in lowkers:
+    t1 = p.select("h3")
+    posisi.append(t1[0].get_text())
 
-t1 = p.select("h3")
-t2 = t1[0].select("a")
-posisi.append(t2[0].get_text())
+    t1 = p.select("a")
+    try:
+        instansi.append(t1[0].get_text())
+    except IndexError:
+        instansi.append("-")
 
-t1 = p.select("p")
-t2 = t1[0].select("a")
+    t2 = p.select("span")
+    try: 
+        gaji.append(t2[1].get_text())
+    except IndexError:
+        gaji.append(t2[0].get_text())
 
-try:
-    instansi.append(t2[0].get_text())
-except:
-    instansi.append("-")
-
-t2 = t1[1].select("span")
-try: 
-    gaji.append(t2[1].get_text())
-except:
-    gaji.append(t2[0].get_text())
-
-
-#Cetak
-    
-    print(posisi)
-    print(instansi)
-    print(gaji)
+# Cetak
+print(posisi)
+print(instansi)
+print(gaji)
