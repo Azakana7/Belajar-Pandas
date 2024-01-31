1ea83ffb41a3c94bf36ac7193683eb644bf9c7bd
Divo Daniel Saputra <155825645+Azakana7@users.noreply.github.com>
2024-01-31 13:02:46 +0700

    Update 3Datasederhana.py

--- 3Datasederhana.py
+++ 3Datasederhana.py
@@ -28,3 +28,63 @@ print(data)
 data = [10, 9, 7, 8, 10, 8] indeks O 
     for elemen in data: 
         print("Elemen ke ", indeks, " = ", elemen) indeks = indeks + 1
+
+
+#Batas
+
+print("4" + "5")
+
+
+nilai1 = 8
+nilai2 = 8
+jumlah = nilai1 + nilai2
+print(jumlah)
+
+
+data = [7, 9, 4, 10, 8, 2]
+print(data[1])
+data[3] = 7
+print(data)
+data.append(4)
+print(data)
+
+
+data = [7, 9, 4, 10, 8, 2]
+indeks = 0
+for elemen in data:
+  print("Elemen ke ", indeks, "=", elemen)
+  indeks = indeks + 1
+
+
+
+data = [12, 6, "Kurniawaty", "5", 7]
+for x in data:
+  print(x/2)
+
+
+
+data = [8, 14, "Alisa", 6, 4]
+for x in data:
+  try:
+    print(x/2)
+  except:
+    print("Bukan bilangan")
+
+
+nil = 4
+if (nil > 0):
+  print("Bilangan positif")
+else:
+  print("Bilangan negatif")
+
+
+import pandas as pd
+
+data = [10, 9, 7, 8, 10]
+siswa = ["Andi", "Budi", "Cindy", "Dedi", "Edi"]
+
+nilai = pd.DataFrame({
+    "Nama": siswa,
+    "Nilai": data
+})
+print(nilai)
